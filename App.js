7d7510e497f4f02494773f55785a20fe9f901daa
Marssix <kasper.heikkinen@gmail.com>
2024-03-27 12:47:49 +0200

    Simple implementation of front page

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home'; 
+import { StyleSheet, View } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"> 
+        <Stack.Screen
+          name="Home"
+          component={Home} 
+          options={{ title: 'Home' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { FlatList, View, TouchableOpacity, Linking, Text } from 'react-native';
+import { Card } from 'react-native-paper';
+import axios from 'axios';
+import styles from '../style/Style';
+import apiKey from '../Firebase/apikey';
 
+const Home = () => {
+    const [recipes, setRecipes] = useState([]);
 
+    useEffect(() => {
+        const fetchRecipeInformation = async () => {
+            try {
+                const response = await axios.get(
+                    'https://api.spoonacular.com/recipes/informationBulk',
+                    {
+                        params: {
+                            ids: '715538,716429',
+                            apiKey: apiKey,
+                        },
+                    }
+                );
+                setRecipes(response.data);
+            } catch (error) {
+                console.error('Error fetching recipe information:', error);
+            }
+        };
 
-export default Home = () => {
-    
-}
+        fetchRecipeInformation();
+    }, []);
+
+    const handleViewRecipe = (url) => {
+        Linking.openURL(url);
+    };
+
+    const renderRecipeItem = ({ item }) => (
+        <TouchableOpacity style={styles.recipeItem} onPress={() => handleViewRecipe(item.spoonacularSourceUrl)}>
+            <Card>
+                <Card.Content>
+                    <Text style={styles.recipeTitle}>{item.title}</Text>
+                </Card.Content>
+                <Card.Cover source={{ uri: item.image }} style={styles.recipeImage} />
+            </Card>
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={recipes}
+                renderItem={renderRecipeItem}
+                keyExtractor={(item) => item.id.toString()}
+                horizontal
+            />
+        </View>
+    );
+};
+
+export default Home;
