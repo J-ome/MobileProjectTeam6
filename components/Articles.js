3cd795dbc4da66aeb891c9d3a1cb7a74f4699bf8
Marssix <kasper.heikkinen@gmail.com>
2024-03-28 11:06:17 +0200

    Added articles

---
+++ components/Articles.js
@@ -0,0 +1,13 @@
+export const articlesData = [
+    {
+        id: 1,
+        title: 'Article 1 Title',
+        content: 'This is the content of article 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+        id: 2,
+        title: 'Article 2 Title',
+        content: 'This is the content of article 2. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    // Add more articles as needed
+];
--- style/Style.js
+++ style/Style.js
@@ -1,6 +1,5 @@
 import { StyleSheet } from "react-native";
 
-
 export default StyleSheet.create({
   container: {
     flex: 1,
@@ -8,12 +7,17 @@ export default StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  recipeContainer: {
+    flex: 1, 
+    marginTop: 10, 
+    marginBottom: 10,
+  },
   recipeItem: {
     width: 200, 
     marginBottom: 15,
     borderRadius: 10,
+    overdlow: 'hidden',
     marginRight: 15,
-    overflow: 'hidden',
     elevation: 3,
   },
   recipeImage: {
@@ -24,5 +28,34 @@ export default StyleSheet.create({
   recipeTitle: {
     fontWeight: 'bold', 
     textAlign: 'center',
-  }
+  },
+  articleItem: {
+    width: '100%',
+    marginBottom: 15,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+  },
+  articleTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  articleContent: {
+    fontSize: 14,
+    lineHeight: 20,
+  },
+  articleContainer: {
+    flex: 1,
+    marginTop: 20,
+    paddingHorizontal: 15,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  articleHeading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
 });
