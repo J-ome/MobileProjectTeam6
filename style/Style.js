2eacfc9af7e4a28d1e7dc6e68f01646efdecf046
Marssix <kasper.heikkinen@gmail.com>
2024-03-28 11:26:06 +0200

    Updates styles

--- style/Style.js
+++ style/Style.js
@@ -1,7 +1,6 @@
 import { StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
-
 export default StyleSheet.create({
   container: {
     flex: 1,
@@ -13,12 +12,17 @@ export default StyleSheet.create({
     marginTop: Constants.statusBarHeight + 10
   },
   recipeItem: {
-    width: 200, 
+    width: 200,
     marginBottom: 15,
     borderRadius: 10,
     marginRight: 15,
     overflow: 'hidden',
     elevation: 3,
+  },
+   recipeContainer: {
+    flex: 1, 
+    marginTop: 10, 
+    marginBottom: 10,
   },
   recipeImage: {
     width: '100%',
@@ -26,7 +30,36 @@ export default StyleSheet.create({
     resizeMode: 'cover',
   },
   recipeTitle: {
-    fontWeight: 'bold', 
+    fontWeight: 'bold',
     textAlign: 'center',
-  }
-  });
+  },
+  articleItem: {
+    width: '100%',
+    marginBottom: 15,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+  },
+  articleTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  articleContent: {
+    fontSize: 14,
+    lineHeight: 20,
+  },
+  articleContainer: {
+    flex: 1,
+    marginTop: 20,
+    paddingHorizontal: 15,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  articleHeading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
