4c80d0e4652ff6f3efc31ce5d55381070f49aa4d
J-ome <n2amjo00@students.oamk.fi>
2024-04-03 10:32:26 +0300

    Recipes page but there is an error with navigation atm, will be solved friday

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,9 +1,19 @@
 
+import React from 'react';
+import { View, Text, Image } from 'react-native';
 
+const Recipe = ({ route }) => {
+  const { recipe } = route.params;
 
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>{recipe.title}</Text>
+      <Image source={{ uri: recipe.image }} style={{ width: 200, height: 200, marginBottom: 10 }} />
+      <Text>{recipe.fullSummary}</Text>
+      <Text>Ready in {recipe.readyInMinutes} minutes</Text>
+      {/* Add more details as needed */}
+    </View>
+  );
+};
 
-const Recipes = () => { 
-
-
-    
-}
+export default Recipe;
