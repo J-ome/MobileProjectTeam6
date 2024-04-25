1b32b19f970d5550f24512424d8a42e66dd582ee
Marssix <kasper.heikkinen@gmail.com>
2024-04-25 12:26:16 +0300

    Donut chart changes

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity, FlatList, ScrollView, SafeAreaView } from 'react-native';
-import { db } from '../firebase/Config';
+import { db } from '../Firebase/Config';
 import { doc, collection, setDoc, deleteDoc, getDoc, addDoc } from 'firebase/firestore';
 import { useAuth } from '../components/AuthContext';
 import { useNavigation } from '@react-navigation/native';
@@ -13,10 +13,11 @@ const Recipe = ({ route }) => {
   const { recipe } = route.params;
   const navigation = useNavigation();
   const favoritesCollection = collection(db, 'favorites');
-  const {user} = useAuth();
+  const { user } = useAuth();
 
   useEffect(() => {
     checkFavoriteStatus();
+    console.log("Ready in minutes ", recipe.readyInMinutes);
   }, []);
 
   const checkFavoriteStatus = async () => {
@@ -55,7 +56,7 @@ const Recipe = ({ route }) => {
       const userDocRef = doc(db, 'users', user.uid);
       const favoritesCollectionRef = collection(userDocRef, 'favorites');
       const recipeDocRef = doc(favoritesCollectionRef, recipe.id.toString());
-      
+
       const recipeSnapshot = await getDoc(recipeDocRef);
       if (!recipeSnapshot.exists()) {
         await setDoc(recipeDocRef, recipe);
@@ -70,9 +71,9 @@ const Recipe = ({ route }) => {
 
   // Prepare data for the donut chart
   const donutChartData = [
-    { name: 'Carbs: ' + Math.round(recipe.nutritionDetails?.carbs?.amount || 0) + ' g', number: Math.round(recipe.nutritionDetails?.carbs?.amount || 0), color: 'green', },
-    { name: 'Protein: ' + Math.round(recipe.nutritionDetails?.protein?.amount || 0) + ' g', number: Math.round(recipe.nutritionDetails?.protein?.amount || 0), color: 'lightpink' },
-    { name: 'Fats: ' + Math.round(recipe.nutritionDetails?.fat?.amount || 0) + ' g', number: Math.round(recipe.nutritionDetails?.fat?.amount || 0), color: 'wheat' },
+    { number: Math.round(recipe.nutritionDetails?.carbs?.amount || 0), color: 'green', },
+    { number: Math.round(recipe.nutritionDetails?.protein?.amount || 0), color: 'lightpink' },
+    { number: Math.round(recipe.nutritionDetails?.fat?.amount || 0), color: 'wheat' },
   ];
   const [isFavorite, setIsFavorite] = useState(false);
 
@@ -89,52 +90,58 @@ const Recipe = ({ route }) => {
   }
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <ScrollView>
-    <View style={styles.recipeItemContainer}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>{recipe.title}</Text>
-      <Image source={{ uri: recipe.image }} style={{ width: 200, height: 200, marginBottom: 10 }} />
-            {/* Display heart icon based on whether recipe is in favorites */}
-            <TouchableOpacity onPress={toggleFavorite} style={styles.favoriteIcon}>
-        {isFavorite ? (
-          <MaterialCommunityIcons name="heart" size={24} color="black" />
-        ) : (
-          <MaterialCommunityIcons name="heart-outline" size={24} color="black" />
-        )}
-      </TouchableOpacity>
-      <Text>Add to favorites</Text>
-      <Text>Ready in {recipe.readyInMinutes} minutes</Text>
-      <Text>Nutrition:</Text>
-      <View style={{ marginLeft: 10 }}>
-        <Text>Carbs: {recipe.nutritionDetails?.carbs?.amount || 0} {recipe.nutritionDetails?.carbs?.unit || ''}</Text>
-        <Text>Protein: {recipe.nutritionDetails?.protein?.amount || 0} {recipe.nutritionDetails?.protein?.unit || ''}</Text>
-        <Text>Fats: {recipe.nutritionDetails?.fat?.amount || 0} {recipe.nutritionDetails?.fat?.unit || ''}</Text>
-        <Text>Kcals: {recipe.nutritionDetails?.kcals?.amount || 0} {recipe.nutritionDetails?.kcals?.unit || ''}</Text>
-      </View>
-      <DonutChart data={donutChartData} />
-      <Text>Ingredients:</Text>
-      <FlatList
-        data={recipe.extendedIngredients || []}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <Text style={{ marginLeft: 10 }}>{`${item.amount} ${item.unit} ${item.name}`}</Text>
-        )}
-      />
+        <View style={styles.recipeItemContainer}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>{recipe.title}</Text>
+          <Image source={{ uri: recipe.image }} style={{ width: 200, height: 200, marginBottom: 10 }} />
+          {/* Display heart icon based on whether recipe is in favorites */}
+          <TouchableOpacity onPress={toggleFavorite} style={styles.favoriteIcon}>
+            {isFavorite ? (
+              <MaterialCommunityIcons name="heart" size={24} color="black" />
+            ) : (
+              <MaterialCommunityIcons name="heart-outline" size={24} color="black" />
+            )}
+          </TouchableOpacity>
+          <Text>Add to favorites</Text>
+          <Text>Ready in {recipe.readyInMinutes} minutes</Text>
+          <DonutChart data={donutChartData} />
+          <View style={{ marginLeft: 10, marginBottom: 20, flexDirection: 'row', alignItems: 'center' }}>
+            <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: 'green', marginRight: 5 }}></View>
+            <Text>Carbs: {recipe.nutritionDetails?.carbs?.amount || 0} {recipe.nutritionDetails?.carbs?.unit || ''}</Text>
+          </View>
+          <View style={{ marginLeft: 10, marginBottom: 20, flexDirection: 'row', alignItems: 'center' }}>
+            <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: 'lightpink', marginRight: 5 }}></View>
+            <Text>Protein: {recipe.nutritionDetails?.protein?.amount || 0} {recipe.nutritionDetails?.protein?.unit || ''}</Text>
+          </View>
+          <View style={{ marginLeft: 10, marginBottom: 20, flexDirection: 'row', alignItems: 'center' }}>
+            <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: 'wheat', marginRight: 5 }}></View>
+            <Text>Fats: {recipe.nutritionDetails?.fat?.amount || 0} {recipe.nutritionDetails?.fat?.unit || ''}</Text>
+          </View>
+          <Text>Ingredients:</Text>
+          <FlatList
+            data={recipe.ingredients || []}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <Text style={{ marginLeft: 10, marginBottom: 5 }}>
+                {`${item.amount} ${item.unit} ${item.name}`}
+              </Text>
+            )}
+          />
+          <Text>Instructions:</Text>
+          <FlatList
+            data={recipe.instructions || []}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+              <Text style={{ marginLeft: 10 }}>{`${index + 1}. ${item}`}</Text>
+            )}
+          />
 
-      <Text>Instructions:</Text>
-      <FlatList
-        data={recipe.instructions || []}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <Text style={{ marginLeft: 10 }}>{`${index + 1}. ${item}`}</Text>
-        )}
-      />
-
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.goBackIcon}>
-        <Ionicons name="chevron-back-outline" size={24} color="black" />
-      </TouchableOpacity>
-    </View>
-    </ScrollView>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={styles.goBackIcon}>
+            <Ionicons name="chevron-back-outline" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
